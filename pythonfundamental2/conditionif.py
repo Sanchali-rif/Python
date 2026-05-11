11742d6d72ea40b36bed8f63724c624e7c93d8f3
Sanchali-rif <sanchalisaha05@gmail.com>
2026-05-11 23:07:37 +0530

    Add age classification, voting eligibility check, and traffic light response scripts

---
+++ pythonfundamental2/conditionif.py
@@ -0,0 +1,7 @@
+age=int(input("enter your age:"))
+if age>=18:
+    print("you can vote")
+    print("you can drive")
+else:
+    print("you cannot vote")
+    print("you cannot drive")
---
+++ pythonfundamental2/traffic.py
@@ -0,0 +1,9 @@
+a=input("enter color:")
+if a=="red":
+    print("Stop")
+elif a=="yellow":
+    print("Look")
+elif a=="green":
+    print("Go")
+else:
+    print("WRONG COLOUR FOR TRAFFIC LIGHTS")
