fca94ddec11dd593821c2c10fcc494056fb8e83e
Sanchali-rif <sanchalisaha05@gmail.com>
2026-05-22 16:56:14 +0530

    Refactor digit printing function and remove unused digit counting logic

--- python_fundamental_2/assignment_problems/assignment_3.py
+++ python_fundamental_2/assignment_problems/assignment_3.py
@@ -2,12 +2,13 @@
 # For example:
 # In 312, there are 3 digits: 3, 1, and 2,
 # and we need to print them.
-def NumOfDigits(n):
-    num=0
-    while n>0:
-        num=num+1
-        n=n//10
-    return num
+def print_digits(n):
+    if n < 10:
+        print(n)
+    else:
+        print_digits(n // 10)  
+        print(n % 10)          
 
-n=int(input("enter the number:"))
-print("so the number of digits in",n,"is",NumOfDigits(n))
+n = int(input("enter num: "))
+print("the digits are-")
+print_digits(n)
--- python_fundamental_2/assignment_problems/assignment_7.py
+++ python_fundamental_2/assignment_problems/assignment_7.py
@@ -1,3 +1,10 @@
 # Design a program to continuously take a number n from the user
 # and print whether it is positive or negative
 # until the user enters "Quit".
+
+
+a=int(input("enter a num:"))
+if(a>=0):
+    print("positive")
+else:
+    print("negative")
