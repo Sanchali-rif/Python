5e4facf07ebc1c1aeb0e58fdaa33630f97a1033a
Sanchali-rif <sanchalisaha05@gmail.com>
2026-05-23 09:30:32 +0530

    Refactor digit printing logic and implement iterative approach; add new empty file for future use

--- python_fundamental_2/assignment_problems/assignment_3.py
+++ python_fundamental_2/assignment_problems/assignment_3.py
@@ -2,13 +2,12 @@
 # For example:
 # In 312, there are 3 digits: 3, 1, and 2,
 # and we need to print them.
-def print_digits(n):
-    if n < 10:
-        print(n)
-    else:
-        print_digits(n // 10)  
-        print(n % 10)          
 
-n = int(input("enter num: "))
+def digits(n):
+    while n>0:
+        print(n%10)
+        n=n//10
+
+n=int(input("enter num:"))
 print("the digits are-")
-print_digits(n)
+digits(n)
