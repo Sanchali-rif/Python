2e4150615a242b58e430e9d93a29b7cedb80260b
Sanchali-rif <sanchalisaha05@gmail.com>
2026-05-25 21:56:16 +0530

    Add string formatting examples to demonstrate variable interpolation

---
+++ python_fundamental_3/str_format.py
@@ -0,0 +1,7 @@
+a=5
+b=10
+sum=a+b
+print("sum is {}".format(sum)) #normal formatting
+print("sum of {} and {} is {}".format(a,b,sum)) #with multiple placeholder
+print("language is {}".format("python"))#normal formatting
+
