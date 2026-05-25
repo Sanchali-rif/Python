37514e8800548d76945a59a2b560589e28dae084
Sanchali-rif <sanchalisaha05@gmail.com>
2026-05-25 23:26:45 +0530

    Add additional examples of string formatting using index and value-based methods

--- python_fundamental_3/str_format.py
+++ python_fundamental_3/str_format.py
@@ -5,3 +5,13 @@ print("sum is {}".format(sum)) #normal formatting
 print("sum of {} and {} is {}".format(a,b,sum)) #with multiple placeholder
 print("language is {}".format("python"))#normal formatting
 
+#index based formatting
+print("sum of {1} and {0} is {2}".format(a,b,sum))
+
+#value based formatting
+print("value of vars {a} & {b}".format(a=5,b=10))
+x=3
+y=4
+#best using f-string
+print(f"sum of {x} and {y} is {x+y}")
+print(f"avg of {x} and {y} is {(x+y)/2}")
