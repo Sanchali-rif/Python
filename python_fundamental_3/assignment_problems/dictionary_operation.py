a99361634771e363ccf5b04b35b3e3d5c95dfab6
Sanchali-rif <sanchalisaha05@gmail.com>
2026-06-06 16:04:47 +0530

    Add implementations for palindrome check, average computation, merging and sorting lists, and even/odd tuple processing

---
+++ python_fundamental_3/assignment_problems/dictionary_operation.py
@@ -0,0 +1,9 @@
+# Create a dictionary where:
+# Keys = student names
+# Values = marks (integer)
+# Write a menu-based program where the user presses a key
+# ('A', 'B', 'C', 'D') depending on the operation they want to perform:
+# A - Add a student
+# B - Update marks
+# C - Search for a student
+# D - Display all students and marks
