dbdc762294361a7bf990407b38f1784ddb9ec9ca
Sanchali-rif <sanchalisaha05@gmail.com>
2026-06-08 22:49:42 +0530

    Add student class with attributes for subject, college, and year

---
+++ python_fundamental_4/oops_concept.py
@@ -0,0 +1,8 @@
+class student:        #this is a class called student where the blueprint of the objects is provided
+    subject="python"  #these are the features of the class that will all the objects have
+    college="RCCIIT"
+    year="2nd year"
+
+stu1=student()        #these are the objects
+stu2=student()
+print(stu1.subject,stu1.college,stu1.year)
