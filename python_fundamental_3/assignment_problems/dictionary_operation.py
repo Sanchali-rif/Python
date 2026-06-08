560a0d61a6addcf1e48ab602480dcede90eed224
Sanchali-rif <sanchalisaha05@gmail.com>
2026-06-08 19:04:58 +0530

    Implement menu-based dictionary operations for student marks management

--- python_fundamental_3/assignment_problems/dictionary_operation.py
+++ python_fundamental_3/assignment_problems/dictionary_operation.py
@@ -7,3 +7,35 @@
 # B - Update marks
 # C - Search for a student
 # D - Display all students and marks
+
+dict={"riya":34}
+while True:
+    operation=input("enter ('A', 'B', 'C', 'D') or 'Q' to quit :")
+    match operation:
+        case "A":
+            name=input("enter name:")
+            marks=int(input("enter marks:"))
+            dict.update({
+                name:marks
+            })
+            print("updated dictionary -",dict)
+        case "B":
+            print("present dictionary -",dict)
+            name=input("whose marks do you want to update:")
+            marks=int(input("enter updated marks:"))
+            dict[name]=marks
+            print("updated dictionary -",dict)
+        case "C":
+            name=input("which student are you searching for:")
+            for i in dict:
+                if name==i:
+                    print(name,":",dict[name])
+                else:
+                    print(name,"does not exist")
+        case "D":
+            print(dict)
+        case "Q":
+            print("Goodbuy!")
+            break
+        case _:
+            print("wrong veriable!")
