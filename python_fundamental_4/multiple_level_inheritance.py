27c7909de58bce9ff3e55e3e0e32c9f1551b4264
Sanchali-rif <sanchalisaha05@gmail.com>
2026-06-12 23:34:50 +0530

    studied pillars of oops

---
+++ python_fundamental_4/multiple_level_inheritance.py
@@ -0,0 +1,10 @@
+class Teacher:
+    def __init__(self,salary):
+        self.salary=salary
+
+class Student:
+    def __init__(self,gpa):
+        self.gpa=gpa
+class TA:
+    def __init__(self,salary,gpa,name):
+        
