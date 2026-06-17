b9a06489e5a4c2799846827ae7b05ba52d141b07
Sanchali-rif <sanchalisaha05@gmail.com>
2026-06-17 09:52:25 +0530

    add the attributes

--- python_fundamental_4/assignment_problems/book_review.py
+++ python_fundamental_4/assignment_problems/book_review.py
@@ -1,2 +1,8 @@
 # Create a class Book with the following attributes: title,author,list of reviews
 # And add methods to: add a new review,count reviews,display all reviews
+
+class Book:
+    def __init__(self,title,author,listOfReviews):
+        self.title=title
+        self.author=author
+        self.listOfReviews=listOfReviews
