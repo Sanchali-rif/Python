f1135d613fe71514aa1314ce127186267987a97d
Sanchali-rif <sanchalisaha05@gmail.com>
2026-07-04 21:58:51 +0530

    Add Python If-Else solution for HackerRank challenge

---
+++ python_problems/python_if_else.py
@@ -0,0 +1,20 @@
+"""
+Problem: Python If-Else
+Platform: HackerRank
+Difficulty: Easy
+Concepts: Conditionals
+
+Problem Link:
+https://www.hackerrank.com/challenges/py-if-else/problem
+"""
+n=int(input())
+if(n%2!=0):
+    print("Weird")
+elif(n%2==0 and 2<=n<=5):
+    print("Not Weird")
+elif(n%2==0 and 6<=n<=20):
+    print("Weird")
+elif(n%2==0 and n>20):
+    print("Not Weird")
+else:
+    print("invalid!")
