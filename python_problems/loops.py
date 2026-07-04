acf944a3093fcce50eefca9fa8bab188dc8cdf18
Sanchali-rif <sanchalisaha05@gmail.com>
2026-07-04 22:16:12 +0530

    Add solutions for HackerRank loops and division problems

---
+++ python_problems/loops.py
@@ -0,0 +1,15 @@
+"""
+Problem: Loops
+Platform: HackerRank
+Difficulty: Easy
+Concepts: for loop, range()
+
+Problem Link:
+https://www.hackerrank.com/challenges/python-loops/problem
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+
+    for i in range(n):
+        print(i ** 2)
---
+++ python_problems/python_division.py
@@ -0,0 +1,16 @@
+"""
+Problem: Python: Division
+Platform: HackerRank
+Difficulty: Easy
+Concepts: Integer Division, Float Division
+
+Problem Link:
+https://www.hackerrank.com/challenges/python-division/problem
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a // b)
+    print(a / b)
