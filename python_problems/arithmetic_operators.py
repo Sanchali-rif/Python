6fc94527da12c72b48f0f05827f4c210e790df15
Sanchali-rif <sanchalisaha05@gmail.com>
2026-07-04 22:03:21 +0530

    Add arithmetic operators solution for HackerRank challenge

---
+++ python_problems/arithmetic_operators.py
@@ -0,0 +1,17 @@
+"""
+Problem: Arithmetic Operators
+Platform: HackerRank
+Difficulty: Easy
+Concepts: Arithmetic Operators
+
+Problem Link:
+https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a + b)
+    print(a - b)
+    print(a * b)
