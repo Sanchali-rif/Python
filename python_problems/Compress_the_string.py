89ebb5ad64dcc1b67c9684a045fb5cc0f89ad3f3
Sanchali-rif <sanchalisaha05@gmail.com>
2026-07-10 12:00:43 +0530

    Add HackerRank Compress the String solution

---
+++ python_problems/Compress_the_string.py
@@ -0,0 +1,13 @@
+"""
+Problem: Compress the String!
+Platform: HackerRank
+Difficulty: Easy
+Concepts: itertools.groupby(), Iterators, Strings
+
+Problem Link:
+https://www.hackerrank.com/challenges/compress-the-string/problem
+"""
+from itertools import groupby
+s=input()
+for key,group in groupby(s):
+    print((len(list(group)),int(key)),end=" ")
